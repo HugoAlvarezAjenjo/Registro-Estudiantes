746fd3e23c9acc6552a114eb308134b69063b19f
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-07-05 16:34:27 +0200

    chore: Migrate to lombok dataclass

--- src/main/java/es/hugoalvarezajenjo/studentmanager/entity/Student.java
+++ src/main/java/es/hugoalvarezajenjo/studentmanager/entity/Student.java
@@ -1,9 +1,11 @@
 package es.hugoalvarezajenjo.studentmanager.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
-@Table(name="STUDENTS")
+@Data
+@Table(name = "STUDENTS")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,39 +19,4 @@ public class Student {
 
     @Column(nullable = false, unique = true)
     private String email;
-
-    public Student() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(final long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(final String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(final String email) {
-        this.email = email;
-    }
 }
